b0cc934072df81b48f697287d2263f2243247354
ctmithilesh <104360312+ctmithilesh@users.noreply.github.com>
2023-08-08 12:40:13 +0530

    new updates

--- queries.sql
+++ queries.sql
@@ -2,3 +2,11 @@ select * from account_type;
 select * from account_status_type;
 select * from interest_savings_rate;
 select * from account_id;
+
+
+select customers_table.first_name, 
+       customers_table.last_name, 
+       account_id.current_blance
+from customers_table 
+join account_id on
+customers_table.account_id = account_id.id 
